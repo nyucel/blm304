cf6275493ca8fef12895354ada941a4ef2af19c9
Emir Kıvrak <emirccus@gmail.com>
2020-05-21 15:00:05 +0300

    Server Side v 1

---
+++ clientside/client_side.py
@@ -0,0 +1,34 @@
+import socket
+
+ 
+
+msgFromClient       = "LIST"
+
+bytesToSend         = str.encode(msgFromClient)
+
+serverAddressPort   = ("127.0.0.1", 42)
+
+bufferSize          = 1024
+
+ 
+
+# Create a UDP socket at client side
+
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+ 
+
+# Send to server using created UDP socket
+
+UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+
+
+ 
+
+msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+
+ 
+
+msg = "Message from Server {}".format(msgFromServer[0])
+
+print(msg)
