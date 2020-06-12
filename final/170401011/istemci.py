3f2cc63c9f3589d0a95f924a06f4df638f7f9a92
berfinokuducu <47542043+berfinokuducu@users.noreply.github.com>
2020-06-12 19:40:51 +0300

    170401011

---
+++ final/170401011/istemci.py
@@ -0,0 +1,25 @@
+#170401011 Berfin Okuducu
+import socket
+import time
+import subprocess
+import shlex
+import pickle
+import datetime
+from datetime import datetime
+host = "127.0.0.1"
+port = 142
+
+baslangic=datetime.now()
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((host,port))
+zaman,address=client.recvfrom(1024)
+zaman=pickle.loads(zaman)
+zaman=zaman[0]
+bitis=datetime.now()
+sonzaman=bitis.microsecond - baslangic.microsecond
+
+zamanekle = datetime(zaman.year,zaman.month,zaman.day,zaman.hour,zaman.minute,zaman.second,zaman.microsecond+sonzaman).isoformat()
+subprocess.call(shlex.split("timedatectl set-ntp false"))
+subprocess.call(shlex.split("sudo date -s '%s'" % zamanekle))
+subprocess.call(shlex.split("sudo hwclock -w"))
+
